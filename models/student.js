4d70deff0a69cfc27e98a6a375450d2496e4c32c
zZissou <kyle.edward.russell@gmail.com>
2016-11-19 12:25:35 -0800

    added student schema and password function for student.js.

--- models/student.js
+++ models/student.js
@@ -2,6 +2,7 @@ var mongoose = require("mongoose"),
     bcrypt = require('bcrypt');
 
 var Schema = mongoose.Schema;
+  Mentor = require('./mentor.js');
 
 var StudentSchema = new Schema({
     name: String,
@@ -12,7 +13,59 @@ var StudentSchema = new Schema({
     areaOfInterest: [String],
     bio: String,
     image: String,
+    // pending [Mentor.schema],
+    //accepted: [Mentor.schema]
 });
+
+StudentSchema.statics.createSecure = function(newUser, callback) {
+  var password = newUser.password;
+  var email = newUser.email;
+  var name = newUser.name;
+  var website = newUser.website;
+  var number = newUser.number;
+  var areaOfInterest = newUser.number;
+  var bio = newUser.bio;
+  var image = newUser.bio;
+  var StudentModel = this;
+
+  bcrypt.genSalt(function(err, salt) {
+    console.log('salt: ', salt);
+    bcrypt.hash(password, salt, function(err, hash) {
+      console.log("In hash function");
+      StudentModel.create({
+        email: email,
+        passwordDigest: hash,
+        name: name,
+        website: website,
+        number: number,
+        areaOfInterest: areaOfInterest,
+        bio: bio,
+        image: image
+        //pending: pending,
+        //accepted: accepted
+      }, callback);
+    });
+  });
+};
+
+StudentSchema.methods.checkPassword = function (password) {
+  return bcrypt.compareSync(password, this.passwordDigest);
+};
+
+StudentSchema.statics.authenticate = function (email, password, callback) {
+  this.findOne({email: email}, function (err, foundUser) {
+    console.log(foundUser);
+
+    if (!foundUser) {
+      console.log('No user with email ' + email);
+      callback("Error: no user found", null);
+    } else if (found.User.checkPassword(password)) {
+      callback(null, foundUser);
+    } else {
+      callback("Error: incorrect password", null);
+    }
+  });
+}
 
 var Student = mongoose.model('Student', StudentSchema);
 module.exports = Student;
